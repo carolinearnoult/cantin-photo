61005d85ec40b00c787a254e8564fa74cab4ee89
caroline arnoult <carolinearnoult@hotmail.fr>
2021-12-22 19:35:38 +0100

    add info css

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,10 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addWatchTarget("./src/css");
     eleventyConfig.addPassthroughCopy("./src/image");
 
-
     return {
+        
+        passthroughFileCopy: true
+
         dir: {
             input: "src",
             output: "public",
