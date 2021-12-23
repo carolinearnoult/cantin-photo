511695bf00c2055801e350d784b2867f9877d737
caroline arnoult <carolinearnoult@hotmail.fr>
2021-12-23 11:18:13 +0100

    activation fichier js dans eleventy dir

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/css");
     eleventyConfig.addWatchTarget("./src/css");
     eleventyConfig.addPassthroughCopy("./src/image");
+    eleventyConfig.addPassthroughCopy("./src/script.js");
 
     return {
 
